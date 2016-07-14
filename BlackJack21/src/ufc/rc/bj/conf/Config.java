082a64eb513f7551d97e41a344d18aca84a0199f
ronildo <ronildo@ronildo-C400-G-BC31P1>
2016-07-14 02:54:04 -0300

    Signed-off-by: ronildo <ronildo@ronildo-C400-G-BC31P1>

--- BlackJack21/src/ufc/rc/bj/conf/Config.java
+++ BlackJack21/src/ufc/rc/bj/conf/Config.java
@@ -6,7 +6,7 @@ public class Config {
 	
 	public static final int PORTA = 65376;
 	public static final String IP = "192.168.1.10";
-	//public static final String IP = "localhost";
+	//public static final String IP = "ec2-52-39-192-30.us-west-2.compute.amazonaws.com";
 	
 	public static final int LARGURA = 500;
 	public static final int ALTURA = 350;
